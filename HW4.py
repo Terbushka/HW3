caa29908becac7624d54bc049fb65b686b725278
Terbushka <53381785+Terbushka@users.noreply.github.com>
2021-03-07 23:30:29 +0200

    Update HW4.py

--- HW4.py
+++ HW4.py
@@ -1,4 +1,5 @@
 # 1. Create a Vehicle class with max_speed and mileage instance attributes
+
 class Vehicle:
     def __init__(self, max_speed, mileage):
         self.max_speed = mileage
@@ -6,6 +7,7 @@ class Vehicle:
 
 
 # 2. Create a child class Bus that will inherit all of the variables and methods of the Vehicle class and will have seating_capacity own method
+
 class Bus(Vehicle):
     def __init__(self, max_speed, mileage, seating_capacity):
         super().__init__(max_speed, mileage)
@@ -14,17 +16,20 @@ class Bus(Vehicle):
     def seating_capacity(self):
         return self.__seating_capacity
 
+
 # 3. Determine which class a given Bus object belongs to (Check type of an object)
+
 bus = Bus(200, 12500, 60)
 print(type(bus))
 print(isinstance(bus, Bus))
 print(issubclass(Bus, Vehicle))
 # 4. Determine if School_bus is also an instance of the Vehicle class
+
 print(isinstance(bus, Vehicle))
 
 
-
 # 5. Create a new class School with get_school_id and number_of_students instance attributes
+
 class School:
     def __init__(self, school_id, number_of_students):
         self.school_id = school_id
@@ -32,7 +37,10 @@ class School:
 
     def get_school_id(self):
         return self.school_id
+
+
 # 6*. Create a new class SchoolBus that will inherit all of the methods from School and Bus and will have its own - bus_school_color
+
 class SchoolBus(School, Bus):
     def __init__(self, school_id, max_speed, mileage, seating_capacity, bus_school_color):
         School.__init__(self, school_id, number_of_students)
@@ -41,11 +49,16 @@ class SchoolBus(School, Bus):
 
     def bus_school_color(self):
         return self.__bus_school_color
+
+
 # 4. Determine if School_bus is also an instance of the Vehicle class
+
 print(issubclass(SchoolBus, Vehicle))
+
 
 # 7. Polymorphism: Create two classes: Bear, Wolf. Both of them should have make_sound method. Create two instances, one of Bear and one of Wolf,
 # make a tuple of it and by using for call their action using the same method.
+
 class Bear:
     def __init__(self, name, age):
         self.name = name
@@ -67,9 +80,11 @@ wolf = Wolf('White Fang', 1)
 for animal in (bear, wolf):
     animal.make_sound()
 
+
 # Magic methods:
 # 8. Create class City with name, population instance attributes, return a new instance only when population > 1500,
 # otherwise return message: "Your city is too small".
+
 class City:
     def __init__(self, name, population):
         self.name = name
@@ -80,10 +95,15 @@ class City:
         if population > 1500:
             return instance
         else:
-            return "Your city is too small"
+            return "Your city is too small."
+
+
 # 9. Override a printable string representation of the City class and return: The population of the city {name} is {population}
+
     def __str__(self):
         return f'The population of the city {self.name} is {self.population}'
+
+
 
 # 10*. Override magic method __add__() to perform the additional action as 'multiply' (*) the value which is greater than 10. And perform this add (+) of two instances.
 
@@ -111,6 +131,7 @@ c5 = Count(9)
 c6 = c4 + c5
 print(c6)
 
+
 # 11. The __call__ method enables Python programmers to write classes where the instances behave like functions and can be called like a function.
 # Create a new class with __call__ method and define this call to return sum.
 
@@ -120,9 +141,11 @@ class CallSum:
 result = CallSum()
 print(result(3, 4, 5))
 
+
 # 12. Making Your Objects Truthy or Falsey Using __bool__().
 # Create class MyOrder with cart and customer instance attributes.
 # Override the __bool__magic method considered to be truthy if the length of the cart list is non-zero.
+
 class MyOrder:
     def __init__(self, cart, customer):
         self.cart = cart
