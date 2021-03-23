66c003b71d71e17a4e256591ed7c12b29ce7bdb8
Terbushka <53381785+Terbushka@users.noreply.github.com>
2021-03-23 16:40:02 +0200

    Update HW8.py

--- HW8.py
+++ HW8.py
@@ -1,30 +1,30 @@
 from __future__ import annotations
+from typing import Dict, Any
 from abc import ABC, abstractmethod
-from typing import Any, Dict
 import random
-import time
 import uuid
+import time
 
 
-@abstractmethod
 class Animal(ABC):
 
-    def __init__(self, strength: int, speed: int):
+    def __init__(self, power: int, speed: int):
         self.id = None
-        self.max_strength = strength
-        self.current_strength = strength
+        self.max_power = power
+        self.current_power = power
         self.speed = speed
 
+    @abstractmethod
     def eat(self, forest: Forest):
-        pass
+        raise NotImplementedError
 
 
 class Predator(Animal):
-    def __init__(self, strength: int, speed: int):
-        super().__init__(strength, speed)
+    def __init__(self, power: int, speed: int):
+        super().__init__(power, speed)
         self.id = None
-        self.max_strength = strength
-        self.current_strength = strength
+        self.max_power = power
+        self.current_power = power
         self.speed = speed
 
     def eat(self, forest: Forest):
@@ -32,43 +32,54 @@ class Predator(Animal):
         if prey.id == self.id:
             print('There are no herbivores in the forest')
         else:
-            if (self.speed > prey.speed) and (self.current_strength > prey.current_strength):
+            if (self.speed > prey.speed) and (self.current_power > prey.current_power):
                 print('Predator find prey')
-                current_strength = min(self.current_strength + self.max_strength * 0.5, self.max_strength)
-                print(f'Predator regained {current_strength} strength')
+                tmp = self.current_power
+                self.current_power = min(self.current_power + self.max_power * 0.5, self.max_power)
+                print(f'Predator regained {self.current_power - tmp} power')
+                forest.animals[prey.id].current_power = 0
             else:
-                self.current_strength = self.current_strength - self.max_strength * 0.3
-                print(f'Predator didn`t catch the prey and lost strength')
-                forest.animals[prey.id].current_strength - 0.3 * forest.animals[prey.id].max_strength
+                print('Predator didn`t catch the prey and lost strength')
+                self.current_power = self.current_power - 0.3 * self.max_power
+                forest.animals[prey.id].current_power = forest.animals[prey.id].current_power - 0.3 * \
+                                                          forest.animals[prey.id].max_power
+
+    def __str__(self):
+        return f'{self.__class__.__name__}'
 
 
 class Herbivorous(Animal):
-    def __init__(self, strength: int, speed: int):
-        super().__init__(strength, speed)
+    def __init__(self, power: int, speed: int):
+        super().__init__(power, speed)
         self.id = None
-        self.max_strength = strength
-        self.current_strength = strength
+        self.max_power = power
+        self.current_power = power
         self.speed = speed
+
+    def __str__(self):
+        return f'{self.__class__.__name__}'
 
     def eat(self, forest: Forest):
         print('Herbivorous eating')
-        current_strength = min(self.current_strength + self.max_strength * 0.5, self.max_strength)
-        print(f'Herbivorous regained {current_strength} strength')
+        tmp = self.current_power
+        self.current_power = min(self.current_power + self.max_power * 0.5, self.max_power)
+        print(f'Herbivorous regained {self.current_power - tmp} power')
 
 
-AnyAnimal: Any[Herbivorous, Predator]
+AnyAnimal = Any[Herbivorous, Predator]
 
 
-class Forest():
+class Forest:
+
     def __init__(self):
         self.animals: Dict[str, AnyAnimal] = dict()
 
     def add_animal(self, animal: AnyAnimal):
-        print('New animal in forest', animal)
+        print('New animal in fores', animal)
         self.animals.update({animal.id: animal})
 
     def remove_animal(self, animal: AnyAnimal):
-        print(f'{animal, id} animal gone')
+        print(animal, 'animal gone')
         self.animals.pop(animal.id)
 
     def any_predator_left(self):
@@ -77,16 +88,16 @@ class Forest():
 
 def animal_generator():
     while True:
-        new_animal = random.choice((Predator(random.randint(25, 100), random.randint(25, 100)),
-                                    Herbivorous(random.randint(25, 100), random.randint(25, 100))))
-        new_animal.id = uuid.uuid4()
-        yield new_animal
+        generated_animal = random.choice((Predator(random.randint(25, 100), random.randint(25, 100)),
+                                          Herbivorous(random.randint(25, 100), random.randint(25, 100))))
+        generated_animal.id = uuid.uuid4()
+        yield generated_animal
 
 
 if __name__ == "__main__":
-    forest = Forest()
     nature = animal_generator()
 
+    forest = Forest()
     for i in range(10):
         animal = next(nature)
         forest.add_animal(animal)
@@ -94,7 +105,7 @@ if __name__ == "__main__":
     while True:
         animal_to_remove = []
         for animal in forest.animals.values():
-            if animal.current_strength < 1:
+            if animal.current_power < 1:
                 animal_to_remove.append(animal.id)
         for animal_id in animal_to_remove:
             forest.remove_animal(forest.animals[animal_id])
